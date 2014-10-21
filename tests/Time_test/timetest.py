761824c5b331e83e1824ef47560245ee343adfe1
Jan Oscar Holbo Rasmussen <jan@kaddu.dk>
2014-10-21 13:31:21 +0100

    timetest.py now asks for frequency of probes (per hour) and the number of probes to perform.

--- tests/Time_test/timetest.py
+++ tests/Time_test/timetest.py
@@ -2,18 +2,24 @@
 # http://www.kaddu.dk/index.php/en/projects/herbot
 # GitHub Repository: https://github.com/JanHolbo/HerBot
 # (C) 2014 by Jan Rasmussen & Kaddu.dk jan at kaddu dot dk
-# This file and others in this pack is - where not otherwise stated - covered by the GPL v2 license
-# Please feel free to fork this project and please submit patches and feature requests to the above email 
+# This file and others in this pack is - where not otherwise stated 
+# covered by the GPL v2 license
+# Please feel free to fork this project and please submit patches and 
+# feature requests to the above email 
 #
 # File: HerBot/tests/Time_test/timetest.py
 # Version: 
 #
-# This is a test-bed python script to determine the accuracy of a non-RTC based clock on Arduino
+# This is a test-bed python script to determine the accuracy 
+# of a non-RTC based clock on Arduino
 # See also the Arduino sketch file HerBot/tests/Time_test/Time_test.ino
 #
 
 import serial
 import time
+import glob
+import sys
+
 
 # Open the serial port. Check which port is being used by the serial link
 #  to the Arduino (ttyUSBx)
@@ -62,16 +68,23 @@ if __name__ == '__main__':
     port = serial.Serial(serPorts[0], baudrate=9600, timeout = 2.0)
 					# select the first available
 
+    print ("HerBot V 0.1-alpha timetest.py script\n")
+    print ("Please enter probing frequency (times per hour) : ")
+    freq = int ( raw_input())
+    print ("Please enter number of probes :")
+    probes = int ( raw_input())
+
+
+    print ("Test will last approx. " + str (probes / freq) + " hours.\n")
+
     print ("T" + str(int(time.time())) + "\n")
     port.write ("T" + str(int(time.time())) + "\n")
 
     count = 0;
-                 # To vary the length of the survey please change the 
-                 # condition below while count<no of data tuples
-    while count<24:
-                 # to vary the frequency of the data tuples please change the 
-                 # delay (in seconds) time.sleep(minuts*60)
-        time.sleep(60*60)
+
+    while count<probes:
+
+        time.sleep((60/freq)*60)
 
         temp = port.readline()
         port.write (chr(7))
